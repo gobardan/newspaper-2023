d3e493a8b7914c278b687ef70c6df3653a6df7db
gobardhan <giwanchdy@gmail.com>
2023-08-11 12:10:19 +0545

    Fix whats_new section

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party library
     "django_summernote",
+    "rest_framework",
     # custom apps
     "newspaper",
     "dlog_app",
+    "api",
 ]
 
 MIDDLEWARE = [
